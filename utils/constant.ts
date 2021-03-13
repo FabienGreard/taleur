327fc5e300a2a96e17b388a62f50c79ea43a0b10
Fabien Gréard <grd.fabien@gmail.com>
2021-03-13 15:39:36 +0100

    🎨 chore: base scaffolding
    🔧 chore: basic setup
    
    -nextjs
    -typescript
    -eslint
    -pretttier
    -husky
    -commitlint with bitmoji
    -tailwind
    -react.intl
    -jest
    -cypress
    
    🎨 chore: Basic Ci
    
    -lhci
    -codeql
    -coverage
    -e2e
    
    🐛 ci: fixe ci
    
    🐛 ci:  yarn install -g has been depreciated
    
    🐛 ci: every steps need an use
    
    🎨 ci: fix lhci
    
    🐛 ci: name jobs and fix
    
    🐛 ci: fix
    
    🐛 ci: fix
    
    🐛 ci: fix
    
    🐛 ci: fix
    
    🐛 ci: fix
    
    🐛 ci: fix
    
    🎨 chore(perf): modern browser
    
    -description meta
    
    :zap: test: fix test
    
    ✅ test: fix header.test
    
    🎨 chore: update
    
    📄 chore(license): GPL-3.0-only

---
+++ utils/constant.ts
@@ -0,0 +1,6 @@
+export const SEO = {
+  title: 'Taleur.io',
+  description: 'Taleur is free scheduling app, plan events and start organizing your next meetings.',
+};
+
+export { version as VERSION } from 'package.json';
