7d3951419c07198c6eb663ed598bb3974cd37559
FabienGreard <grd.fabien@gmail.com>
2021-03-14 03:34:44 +0100

    🎨 chore(perf): modern browser
    -description meta

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,17 @@ import Head from 'next/head';
 
 interface HeaderProps {
   title?: string;
+  description?: string;
 }
 
-export default function Header({ title = 'NextJs Boilerplate' }: HeaderProps) {
+export default function Header({
+  title = 'Taleur.io',
+  description = 'Taleur.io is a free scheduling app, plan events and start organizing your next meetings, with synching everything on your agenda.',
+}: HeaderProps) {
   return (
     <Head>
       <title>{title}</title>
+      <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
